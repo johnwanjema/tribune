1c58d07dd4d1068f4ce0a1a0e93416bb3206f289
johnwanjema <jonwanjema@gmail.com>
2019-07-22 17:32:34 +0300

    added welcome email

--- news/models.py
+++ news/models.py
@@ -2,6 +2,10 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
--- news/views.py
+++ news/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 import datetime as dt
 from django.http  import HttpResponse,Http404
-from .models import Article
+from .models import Article,NewsLetterRecipients
+from .forms import NewsLetterForm
+from .emails import send_welcome_email
 
 # Create your views here.
 def welcome(request):
@@ -13,7 +15,20 @@ def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
     print(news)
-    return render(request, 'all-news/today-news.html', {"date": date,"news" :news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 
 def convert_dates(dates):
