a8c2954b4afb1d290b9909c1a83515812c0c60e0
johnwanjema <jonwanjema@gmail.com>
2019-08-02 08:43:41 +0300

    getting a single itrem

--- news/views.py
+++ news/views.py
@@ -112,7 +112,35 @@ class MerchList(APIView):
             serializers.save()
             permission_classes = (IsAdminOrReadOnly,)
             return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)   
+
+class MerchDescription(APIView):
+    def get_merch(self, pk):
+        try:
+            return MoringaMerch.objects.get(pk=pk)
+        except MoringaMerch.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            permission_classes = (IsAdminOrReadOnly,)
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        merch.delete()
+        permission_classes = (IsAdminOrReadOnly,)
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
 
 
